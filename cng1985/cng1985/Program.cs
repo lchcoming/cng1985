4388038fe10b4e43b527e2788457c29e580bbfbb
cng1985 <cng1985@4a583d68-532f-11de-8b28-e1b452efc53a>
2009-06-07 09:08:10 +0000

    奇怪的事件，事件很神奇

--- cng1985/cng1985/Program.cs
+++ cng1985/cng1985/Program.cs
@@ -11,6 +11,21 @@ namespace cng1985
         {
             Console.Write("haha");
             Console.Write("hello ");
+            WebWork web = new WebWork();
+            web.Run();
+          
+            for (int i = 0; i < 1000; i++)
+            {
+                web.Work += new EventHandler(web_Work);
+            }
+            web.Work+=new EventHandler(web_Work);
+            web.Test();
+            Console.Read();
+        }
+
+        static void web_Work(object sender, EventArgs e)
+        {
+            Console.Write("123\n");
         }
     }
 }
