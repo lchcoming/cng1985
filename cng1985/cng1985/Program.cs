86bd39754a86c460d92e34d89119048a4c00ce6d
cng1985 <cng1985@4a583d68-532f-11de-8b28-e1b452efc53a>
2009-06-10 07:50:31 +0000

    妈妈的，怎么有个工程没了

--- cng1985/cng1985/Program.cs
+++ cng1985/cng1985/Program.cs
@@ -4,17 +4,56 @@ using System.Linq;
 using System.Text;
 using System.Net;
 using System.Threading;
+using System.Security.Cryptography;
+using System.IO;
 namespace cng1985
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.Write("haha");
-            Console.Write("hello ");
-            WebWork web = new WebWork();
-            web.Run();
+            DESCryptoServiceProvider key = new DESCryptoServiceProvider();
 
+            // Encrypt a string to a byte array.
+            byte[] buffer = Encrypt("This is some plaintext! 我是你爷爷", key);
+            Console.WriteLine(Encoding.Unicode.GetString(buffer));
+            // Decrypt the byte array back to a string.
+            string plaintext = Decrypt(buffer, key);
+
+            // Display the plaintext value to the console.
+            Console.WriteLine(plaintext);
+
+            string source = "Hello World!";
+
+            string hash = getMd5Hash(source);
+            Console.WriteLine(hash.Length);
+
+            Console.WriteLine("The MD5 hash of " + source + " is: " + hash + ".");
+
+            Console.WriteLine("Verifying the hash...");
+
+            if (verifyMd5Hash(source, hash))
+            {
+                Console.WriteLine("The hashes are the same.");
+            }
+            else
+            {
+                Console.WriteLine("The hashes are not same.");
+            }
+            if (verifyMd5HashCryp(source, hash))
+            {
+                Console.WriteLine("The hashes are the same.");
+            }
+            else
+            {
+                Console.WriteLine("The hashes are not same.");
+            }
+            Console.WriteLine(getMd5HashSHA512Cng(source));
+            Console.Read();
+        }
+
+        private static void Visiat(WebWork web)
+        {
             for (int i = 0; i < 50; i++)
             {
                 web.Work += new EventHandler(web_Work);
@@ -65,17 +104,11 @@ namespace cng1985
                         catch (Exception e)
                         {
                             Console.WriteLine(e.Message);
-                            
+
                         }
                     }
                 });
             }
-            web.Work += new EventHandler(web_Work);
-            web.Test();
-            ThreadStart star = () => { };
-
-            web.Run();
-            Console.Read();
         }
 
         static void web_Work(object sender, EventArgs e)
@@ -90,5 +123,225 @@ namespace cng1985
             //Console.WriteLine("访问成功");
             //respon.Close();
         }
+        static string getMd5Hash(string input)
+        {
+            // Create a new instance of the MD5CryptoServiceProvider object.
+            MD5 md5Hasher = MD5.Create();
+
+            // Convert the input string to a byte array and compute the hash.
+            byte[] data = md5Hasher.ComputeHash(Encoding.Default.GetBytes(input));
+
+            // Create a new Stringbuilder to collect the bytes
+            // and create a string.
+            StringBuilder sBuilder = new StringBuilder();
+
+            // Loop through each byte of the hashed data 
+            // and format each one as a hexadecimal string.
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+
+            // Return the hexadecimal string.
+            return sBuilder.ToString();
+        }
+        static bool verifyMd5Hash(string input, string hash)
+        {
+            // Hash the input.
+            string hashOfInput = getMd5Hash(input);
+
+            // Create a StringComparer an comare the hashes.
+            StringComparer comparer = StringComparer.OrdinalIgnoreCase;
+
+            if (0 == comparer.Compare(hashOfInput, hash))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        static string getMd5HashCryp(string input)
+        {
+            // Create a new instance of the MD5CryptoServiceProvider object.
+            MD5CryptoServiceProvider md5Hasher = new MD5CryptoServiceProvider();
+
+            // Convert the input string to a byte array and compute the hash.
+            byte[] data = md5Hasher.ComputeHash(Encoding.Default.GetBytes(input));
+
+            // Create a new Stringbuilder to collect the bytes
+            // and create a string.
+            StringBuilder sBuilder = new StringBuilder();
+
+            // Loop through each byte of the hashed data 
+            // and format each one as a hexadecimal string.
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+
+            // Return the hexadecimal string.
+            return sBuilder.ToString();
+        }
+
+        // Verify a hash against a string.
+        static bool verifyMd5HashCryp(string input, string hash)
+        {
+            // Hash the input.
+            string hashOfInput = getMd5HashCryp(input);
+
+            // Create a StringComparer an compare the hashes.
+            StringComparer comparer = StringComparer.OrdinalIgnoreCase;
+
+            if (0 == comparer.Compare(hashOfInput, hash))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        static string getMd5HashCng(string input)
+        {
+            // Create a new instance of the MD5CryptoServiceProvider object.
+            MD5Cng md5Hasher = new MD5Cng();
+
+            // Convert the input string to a byte array and compute the hash.
+            byte[] data = md5Hasher.ComputeHash(Encoding.Default.GetBytes(input));
+
+            // Create a new Stringbuilder to collect the bytes
+            // and create a string.
+            StringBuilder sBuilder = new StringBuilder();
+
+            // Loop through each byte of the hashed data 
+            // and format each one as a hexadecimal string.
+            Console.WriteLine(data.Length);
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+
+            // Return the hexadecimal string.
+            return sBuilder.ToString();
+        }
+
+        // Verify a hash against a string.
+        static bool verifyMd5HashCng(string input, string hash)
+        {
+            // Hash the input.
+            string hashOfInput = getMd5HashCng(input);
+
+            // Create a StringComparer an compare the hashes.
+            StringComparer comparer = StringComparer.OrdinalIgnoreCase;
+
+            if (0 == comparer.Compare(hashOfInput, hash))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        static string getMd5HashSHA512Cng(string input)
+        {
+            // Create a new instance of the MD5CryptoServiceProvider object.
+            SHA512Cng md5Hasher = new SHA512Cng();
+
+            // Convert the input string to a byte array and compute the hash.
+            byte[] data = md5Hasher.ComputeHash(Encoding.Default.GetBytes(input));
+
+            // Create a new Stringbuilder to collect the bytes
+            // and create a string.
+            StringBuilder sBuilder = new StringBuilder();
+
+            // Loop through each byte of the hashed data 
+            // and format each one as a hexadecimal string.
+            Console.WriteLine(data.Length);
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+
+            // Return the hexadecimal string.
+            return sBuilder.ToString();
+        }
+
+        // Verify a hash against a string.
+        static bool verifyMd5HashSHA512Cng(string input, string hash)
+        {
+            // Hash the input.
+            string hashOfInput = getMd5HashSHA512Cng(input);
+
+            // Create a StringComparer an compare the hashes.
+            StringComparer comparer = StringComparer.OrdinalIgnoreCase;
+
+            if (0 == comparer.Compare(hashOfInput, hash))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        // Encrypt the string.
+        public static byte[] Encrypt(string PlainText, SymmetricAlgorithm key)
+        {
+            // Create a memory stream.
+            MemoryStream ms = new MemoryStream();
+
+            // Create a CryptoStream using the memory stream and the 
+            // CSP DES key.  
+            CryptoStream encStream = new CryptoStream(ms, key.CreateEncryptor(), CryptoStreamMode.Write);
+
+            // Create a StreamWriter to write a string
+            // to the stream.
+            StreamWriter sw = new StreamWriter(encStream);
+
+            // Write the plaintext to the stream.
+            sw.WriteLine(PlainText);
+
+            // Close the StreamWriter and CryptoStream.
+            sw.Close();
+            encStream.Close();
+
+            // Get an array of bytes that represents
+            // the memory stream.
+            byte[] buffer = ms.ToArray();
+            // Close the memory stream.
+            ms.Close();
+
+            // Return the encrypted byte array.
+            return buffer;
+        }
+
+        // Decrypt the byte array.
+        public static string Decrypt(byte[] CypherText, SymmetricAlgorithm key)
+        {
+            // Create a memory stream to the passed buffer.
+            MemoryStream ms = new MemoryStream(CypherText);
+
+            // Create a CryptoStream using the memory stream and the 
+            // CSP DES key. 
+            CryptoStream encStream = new CryptoStream(ms, key.CreateDecryptor(), CryptoStreamMode.Read);
+
+            // Create a StreamReader for reading the stream.
+            StreamReader sr = new StreamReader(encStream);
+
+            // Read the stream as a string.
+            string val = sr.ReadLine();
+            // Close the streams.
+            sr.Close();
+            encStream.Close();
+            ms.Close();
+
+            return val;
+        }
+
     }
 }
