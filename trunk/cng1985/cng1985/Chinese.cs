ef676160c6567b668b019da0c3769259df726372
cng1985 <cng1985@4a583d68-532f-11de-8b28-e1b452efc53a>
2009-06-10 07:50:31 +0000

    妈妈的，怎么有个工程没了

---
+++ trunk/cng1985/cng1985/Chinese.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace cng1985
+{
+    class Chinese
+    {
+        public void Run()
+        {
+            string ada = "你是谁";
+            char[] d = ada.ToCharArray();
+            foreach (var item in d)
+            {
+                Console.WriteLine(item.GetTypeCode());
+                Console.WriteLine((char)((int)item - 43));
+            }
+            char a = '陈';
+            int aa = (int)a;
+            Console.WriteLine(aa);
+            char b = '陳';
+            int bb = (int)b;
+            Console.WriteLine(bb);
+            Console.WriteLine(bb - aa);
+           
+        }
+
+
+    }
+}
