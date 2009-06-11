001a31b18664eb0926d1111527087fc04689ad2e
cng1985 <cng1985@4a583d68-532f-11de-8b28-e1b452efc53a>
2009-06-11 12:28:24 +0000

    新建一个工程，为了自己寻找工作

---
+++ cng1985/Work/MainForm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Work.UI;
+namespace Work
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        private Login login;
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            login = new Login();
+            login.ShowDialog();
+            if (!login.LoginStatus)
+            {
+                this.Close();
+            }
+            
+        }
+    }
+}
---
+++ cng1985/Work/UI/Login.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Work.UI
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+        private bool _loginStatus;
+
+        public bool LoginStatus
+        {
+            get { return _loginStatus; }
+            set { _loginStatus = value; }
+        }
+        private string _userName;
+
+        public string UserName
+        {
+            get { return _userName; }
+            set { _userName = value; }
+        }
+        private string _userPassword;
+
+        public string UserPassword
+        {
+            get { return _userPassword; }
+            set { _userPassword = value; }
+        }
+
+        private void Loginbutton_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text == "ada")
+            {
+                LoginStatus = true;
+                this.Close();
+            }
+            else {
+                MessageBox.Show("用户密码错误");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            LoginStatus = false;
+            this.Close();
+        }
+    }
+}
