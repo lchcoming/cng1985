e7c64ef39b4c9d513a59e198a20038ba9654d2f0
cng1985 <cng1985@4a583d68-532f-11de-8b28-e1b452efc53a>
2009-10-22 02:48:33 +0000

    抽取工具箱和主要api

---
+++ trunk/jbpm/src/com/ada/common/util/DateUtil.java
@@ -0,0 +1,12 @@
+package com.ada.common.util;
+
+import java.util.Date;
+
+import com.ada.common.factory.DateFormatFactory;
+
+
+public class DateUtil {
+	public static String toChineseDate(Date time){
+		return DateFormatFactory.createChineseDate().format(time);
+	}
+}
