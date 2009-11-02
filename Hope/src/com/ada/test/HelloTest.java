8311d67b636bfaf23c98e69d78343ff4ec43661f
cng1985 <cng1985@4a583d68-532f-11de-8b28-e1b452efc53a>
2009-11-02 13:22:09 +0000

    添加juint4

---
+++ Hope/src/com/ada/test/HelloTest.java
@@ -0,0 +1,31 @@
+/** 
+ * CopyRright (c)1985-2009:ada.young <br />                             
+ * Project: Hope<br />                                           
+ * Module ID:    <br />   
+ * Comments:            <br />                                  
+ * JDK version used:<JDK1.6><br />                                 
+ * Namespace:com.ada.test<br />                             
+ * Author：ada.young <br />                  
+ * Create Date：  2009-11-2<br />   
+ * Modified By：ada.young          <br />                                
+ * Modified Date:2009-11-2        <br />                               
+ * Why & What is modified <br />   
+ * Version: 1.01         <br />       
+ */
+package com.ada.test;
+
+
+import org.junit.After;
+import org.junit.Before;
+
+public class HelloTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+}
