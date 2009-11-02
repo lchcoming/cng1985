91b0a1e9ce28af73f66495f417bb4f95b5fb36fd
cng1985 <cng1985@4a583d68-532f-11de-8b28-e1b452efc53a>
2009-11-02 02:09:11 +0000

    添加缓存 if (temp == null) { 			temp = query.getResultList(); 		}

--- Hope/src/com/ada/data/adapter/ArticleEMFAdapter.java
+++ Hope/src/com/ada/data/adapter/ArticleEMFAdapter.java
@@ -12,7 +12,8 @@ import com.ada.data.page.PageBean;
 import com.ada.data.page.Pager;
 import com.helper.PageBeans;
 import com.helper.PageResult;
-public class ArticleEMFAdapter implements ArticleAdapter{
+
+public class ArticleEMFAdapter implements ArticleAdapter {
 	public void addArticle(Article art) {
 		EntityManager em = EMF.get().createEntityManager();
 		try {
@@ -24,22 +25,24 @@ public class ArticleEMFAdapter implements ArticleAdapter{
 
 	public void updateArticle(Article art) {
 		EntityManager em = EMF.get().createEntityManager();
-	    try {
-	    	long key=art.getId();
-	    	Article f = em.find(Article.class, key);
-	    } finally {
-	      em.close();
-	    }
+		try {
+			long key = art.getId();
+			Article f = em.find(Article.class, key);
+		} finally {
+			em.close();
+		}
 	}
+
 	public void findArticle(long key) {
 		EntityManager em = EMF.get().createEntityManager();
-	    try {
-	    	Article f = em.find(Article.class, key);
-	      
-	    } finally {
-	      em.close();
-	    }
+		try {
+			Article f = em.find(Article.class, key);
+
+		} finally {
+			em.close();
+		}
 	}
+
 	public void deleteArticle(Article art) {
 		EntityManager em = EMF.get().createEntityManager();
 		try {
@@ -52,47 +55,50 @@ public class ArticleEMFAdapter implements ArticleAdapter{
 	}
 
 	public List<Article> findArticle(String query) {
-		 EntityManager em = EMF.get().createEntityManager();
-		    try {
-		      List<Article> flights = em.createQuery(query).getResultList();
-		      // force all results to be pulled back before we close the entity manager
-		      flights.size();
-		      return flights;
-		    } finally {
-		      em.close();
-		    }
-	}
-    public int findsize(String query){
-    	return findArticle(query).size();
-    }
-    public List<Article> pageArticle(String query,int curpage,int pagesize) {
-    	PageResult result=new PageResult();
-    	 EntityManager em = EMF.get().createEntityManager();
+		EntityManager em = EMF.get().createEntityManager();
 		try {
-			
-			Query qu=em.createQuery(query);
-			int max=0;
-			int all=0;
-			all=findsize(query);
-			max=all/pagesize;
-			if(all%pagesize!=0){
-				max=max+1;
+			List<Article> flights = em.createQuery(query).getResultList();
+			// force all results to be pulled back before we close the entity
+			// manager
+			flights.size();
+			return flights;
+		} finally {
+			em.close();
+		}
+	}
+
+	public int findsize(String query) {
+		return findArticle(query).size();
+	}
+
+	public List<Article> pageArticle(String query, int curpage, int pagesize) {
+		PageResult result = new PageResult();
+		EntityManager em = EMF.get().createEntityManager();
+		try {
+
+			Query qu = em.createQuery(query);
+			int max = 0;
+			int all = 0;
+			all = findsize(query);
+			max = all / pagesize;
+			if (all % pagesize != 0) {
+				max = max + 1;
 			}
-			//�߽��ж�
-			if(pagesize<0){
-				pagesize=10;
+			// �߽��ж�
+			if (pagesize < 0) {
+				pagesize = 10;
 			}
-			if(curpage>max){
-				curpage=max;
+			if (curpage > max) {
+				curpage = max;
 			}
-			if(curpage<1){
-				curpage=1;
+			if (curpage < 1) {
+				curpage = 1;
 			}
-			int star=(curpage-1)*pagesize;
+			int star = (curpage - 1) * pagesize;
 			qu.setFirstResult(star);
 			qu.setMaxResults(pagesize);
 			List<Article> flights = (List<Article>) qu.getResultList();
-					
+
 			// Force all results to be pulled back before we close the entity
 			// manager.
 			// We could have also called pm.detachCopyAll()
@@ -102,39 +108,34 @@ public class ArticleEMFAdapter implements ArticleAdapter{
 			em.close();
 		}
 	}
+
 	// ��ȡ��ҳ����
-	public PageResult getPageResult(String hsql, String currentPage, String pagerMethod,int pageSize)
-	{
+	public PageResult getPageResult(String hsql, String currentPage,
+			String pagerMethod, int pageSize) {
 		// ����pager�������ڴ���ҳ��
 		int totalRows = 0;
 		List resultList = null;
 		PageBeans pager = null;
-		 EntityManager em = EMF.get().createEntityManager();
+		EntityManager em = EMF.get().createEntityManager();
 		Query query = em.createQuery(hsql);
 		totalRows = findsize(hsql); // ȡ���ܼ�¼��
-		pager = new PageBeans(totalRows,pageSize);
-       
+		pager = new PageBeans(totalRows, pageSize);
+
 		// ���ǰҳ��Ϊ�գ���ʾΪ�״β�ѯ��ҳ
 		// ���Ϊ�գ���ˢ��pager�������뵱ǰҳ�ŵ���Ϣ
-		if (currentPage != null)
-		{
+		if (currentPage != null) {
 			pager.refresh(Integer.parseInt(currentPage));
 		}
-//		System.out.println("pagerMethod========"+pagerMethod);
+		// System.out.println("pagerMethod========"+pagerMethod);
 		// ��ȡ��ǰִ�еķ�������ҳ��ǰһҳ����һҳ��βҳ��
-		if (pagerMethod != null)
-		{
-			if (pagerMethod.equals("first"))
-			{
+		if (pagerMethod != null) {
+			if (pagerMethod.equals("first")) {
 				pager.first();
-			} else if (pagerMethod.equals("previous"))
-			{
+			} else if (pagerMethod.equals("previous")) {
 				pager.previous();
-			} else if (pagerMethod.equals("next"))
-			{
+			} else if (pagerMethod.equals("next")) {
 				pager.next();
-			} else if (pagerMethod.equals("last"))
-			{
+			} else if (pagerMethod.equals("last")) {
 				pager.last();
 			}
 		}
@@ -142,87 +143,87 @@ public class ArticleEMFAdapter implements ArticleAdapter{
 		query.setFirstResult(pager.getStartRow());
 		// ȡ��pageSize����¼
 		query.setMaxResults(pager.getPageSize());
-		
-		if(query.getResultList().size()>0)
-		{
+
+		if (query.getResultList().size() > 0) {
 			resultList = query.getResultList();
 		}
-		
-		
-		
-		//�ѷ�ҳ��ѯ�Ľ��Ͷ������PagerResult��
-		
-		if(resultList!=null)
-		{
-			PageResult pagerResult=new PageResult(); 		
+
+		// �ѷ�ҳ��ѯ�Ľ��Ͷ������PagerResult��
+
+		if (resultList != null) {
+			PageResult pagerResult = new PageResult();
 			pagerResult.setPageBean(pager);
 			pagerResult.setResultList(resultList);
 			return pagerResult;
-		}
-		else
-		{
-			//System.out.println("resultList================1111111======"+resultList);
+		} else {
+			// System.out.println("resultList================1111111======"+resultList);
 			return null;
 		}
-		
+
 	}
-   public PageBeans getPageBean(String hsql,String currentPage, String pagerMethod,int pageSize){
-	   int totalRows = 0;
-	   totalRows = findsize(hsql);
-	   PageBeans result=new PageBeans(totalRows, pageSize);
-	   result.setQuery(hsql);
-		if (currentPage != null)
-		{
+
+	public PageBeans getPageBean(String hsql, String currentPage,
+			String pagerMethod, int pageSize) {
+		int totalRows = 0;
+		totalRows = findsize(hsql);
+		PageBeans result = new PageBeans(totalRows, pageSize);
+		result.setQuery(hsql);
+		if (currentPage != null) {
 			result.refresh(Integer.parseInt(currentPage));
 		}
-	   if (pagerMethod != null)
-		{
-			if (pagerMethod.equals("first"))
-			{
+		if (pagerMethod != null) {
+			if (pagerMethod.equals("first")) {
 				result.first();
-			} else if (pagerMethod.equals("previous"))
-			{
+			} else if (pagerMethod.equals("previous")) {
 				result.previous();
-			} else if (pagerMethod.equals("next"))
-			{
+			} else if (pagerMethod.equals("next")) {
 				result.next();
-			} else if (pagerMethod.equals("last"))
-			{
+			} else if (pagerMethod.equals("last")) {
 				result.last();
 			}
 		}
-	   return result;
-   }
-   public List pageArticle(PageBeans page){
-	   return pageArticle(page.getQuery(),page.getCurrentPage(),page.getPageSize());
-   }
-  public Pager getpage(String hql, int currentPage, int pageSize){
-	  int totalRows = 0;
+		return result;
+	}
+
+	public List pageArticle(PageBeans page) {
+		return pageArticle(page.getQuery(), page.getCurrentPage(), page
+				.getPageSize());
+	}
+
+	public Pager getpage(String hql, int currentPage, int pageSize) {
+		int totalRows = 0;
 		List resultList = null;
 		PageBean pager = null;
-		 EntityManager em = EMF.get().createEntityManager();
+		EntityManager em = EMF.get().createEntityManager();
 		Query query = em.createQuery(hql);
-		List temp=query.getResultList();
+		if (temp == null) {
+			temp = query.getResultList();
+		}
+
 		totalRows = (temp.size()); // 取得总计录数
 
 		pager = new PageBean(pageSize, currentPage, totalRows);
 		// 从当前页记录数开始
 
-//		query.setFirstResult((pager.getCurrentPage() - 1) * pageSize);
-//		// 取出pageSize个记录
-//
-//		query.setMaxResults(pager.getPageSize());
-//		if (query.getResultList().size() > 0) {
-//			resultList = query.getResultList();
-//		}
+		// query.setFirstResult((pager.getCurrentPage() - 1) * pageSize);
+		// // 取出pageSize个记录
+		//
+		// query.setMaxResults(pager.getPageSize());
+		// if (query.getResultList().size() > 0) {
+		// resultList = query.getResultList();
+		// }
 		// 把分页查询的结果和对象放入PagerResult中
-		resultList=temp.subList(pager.getStartRow(), pager.getEndRow());
+		if (temp.size() > 1) {
+			resultList = temp.subList(pager.getStartRow(), pager.getEndRow());
+		}
 		Pager pagerResult = new Pager();
 		pagerResult.setPageBean(pager);
 		pagerResult.setResultList(resultList);
-		if(em.isOpen()){
+		if (em.isOpen()) {
 			em.close();
 		}
 		return pagerResult;
-  }
+	}
+
+	private static List temp;
 }
