8ade293e4eda87ed13331e26cf718b8a2341e055
cng1985 <cng1985@4a583d68-532f-11de-8b28-e1b452efc53a>
2009-11-05 08:50:49 +0000

    加油，ada.young

---
+++ Hope/src/com/ada/dao/proxy/EMFProxy.java
@@ -0,0 +1,19 @@
+/** 
+ * CopyRright (c)1985-2009:ada.young <br />                             
+ * Project: Hope<br />                                           
+ * Module ID:    <br />   
+ * Comments:            <br />                                  
+ * JDK version used:<JDK1.6><br />                                 
+ * Namespace:com.ada.dao.proxy<br />                             
+ * Author：ada.young <br />                  
+ * Create Date：  2009-11-5<br />   
+ * Modified By：ada.young          <br />                                
+ * Modified Date:2009-11-5        <br />                               
+ * Why & What is modified <br />   
+ * Version: 1.01         <br />       
+ */
+package com.ada.dao.proxy;
+
+public class EMFProxy {
+
+}
