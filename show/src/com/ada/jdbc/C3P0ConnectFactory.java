551ecb8ba50c49d354c8732c95fa64986b608f2d
cng1985 <cng1985@4a583d68-532f-11de-8b28-e1b452efc53a>
2009-11-10 04:51:20 +0000

    实现了ConnectFactory

--- show/src/com/ada/jdbc/C3P0ConnectFactory.java
+++ show/src/com/ada/jdbc/C3P0ConnectFactory.java
@@ -15,25 +15,65 @@
 package com.ada.jdbc;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import com.ada.common.util.Config;
 
 public class C3P0ConnectFactory implements ConnectFactory {
+	private static Log log = LogFactory.getLog(C3P0ConnectFactory.class);
 
-	@Override
 	public Connection creadteDB2() {
-		// TODO Auto-generated method stub
-		return null;
+		Connection connection = null;
+		Properties properties = Config.loadProperties(
+				"/properties/db2.properties", "db2 properties");
+		connection = createConnect(properties);
+
+		return connection;
+	}
+
+	Connection createConnect(Properties properties) {
+		Connection connection = null;
+		if (null == properties || properties.size() < 4) {
+			log.error("加载资源失败");
+		} else {
+			log.info("加载资源成功");
+			String driver = properties.getProperty("driver");
+			String url = properties.getProperty("url");
+			String username = properties.getProperty("username");
+			String password = properties.getProperty("password");
+			try {
+				Class.forName(driver);
+				connection = DriverManager.getConnection(url, username,
+						password);
+			} catch (SQLException e) {
+				log.error("fail", e);
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return connection;
+	}
+
+	public Connection creadteMysql() {
+		Connection connection = null;
+		Properties properties = Config.loadProperties(
+				"/properties/mysql.properties", "mysql properties");
+		connection = createConnect(properties);
+		return connection;
 	}
 
 	@Override
 	public Connection creadteMSsql() {
-		// TODO Auto-generated method stub
-		return null;
+		Connection connection = null;
+		Properties properties = Config.loadProperties(
+				"/properties/mssql.properties", "mysql properties");
+		connection = createConnect(properties);
+		return connection;
 	}
-
-	@Override
-	public Connection creadteMysql() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
 }
