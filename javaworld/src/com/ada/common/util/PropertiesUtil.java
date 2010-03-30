040253f538f311582e2ae2971a3a87bd98ae4957
cng1985 <cng1985@4a583d68-532f-11de-8b28-e1b452efc53a>
2010-03-30 06:52:47 +0000

    添加了oscache里面的代码

--- javaworld/src/com/ada/common/util/PropertiesUtil.java
+++ javaworld/src/com/ada/common/util/PropertiesUtil.java
@@ -20,9 +20,14 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.URL;
 import java.util.Properties;
+import java.util.logging.Logger;
+
 
 public class PropertiesUtil {
+	private static Logger log;
+
 	public static Properties readProperties(String filename) {
 		Properties properties = new Properties();
 		InputStream in = null;
@@ -83,4 +88,61 @@ public class PropertiesUtil {
 
 		return properties;
 	}
+    /**
+     * Load the properties from the specified URL.
+     * @param url a non null value of the URL to the properties
+     * @param info additional logger information if the properties can't be read
+     * @return the loaded properties specified by the URL
+     * @since 2.4
+     */
+    public static Properties loadProperties(URL url, String info) {
+        log.info("OSCache: Getting properties from URL " + url + " for " + info);
+
+        Properties properties = new Properties();
+        InputStream in = null;
+
+        try {
+            in = url.openStream();
+            properties.load(in);
+            log.info("OSCache: Properties read " + properties);
+        } catch (Exception e) {
+            log.severe("OSCache: Error reading from " + url+ e);
+            log.severe("OSCache: Ensure the properties information in " + url+ " is readable and in your classpath.");
+        } finally {
+            try {
+                in.close();
+            } catch (IOException e) {
+                log.warning("OSCache: IOException while closing InputStream: " + e.getMessage());
+            }
+        }
+        
+        return properties;
+    }
+
+    /**
+     * Load the specified properties file from the classpath. If the file
+     * cannot be found or loaded, an error will be logged and no
+     * properties will be set.
+     * @param filename the properties file with path
+     * @param info additional logger information if file can't be read
+     * @return the loaded properties specified by the filename
+     * @since 2.4
+     */
+    public static Properties loadProperties(String filename, String info) {
+        URL url = null;
+        
+        ClassLoader threadContextClassLoader = Thread.currentThread().getContextClassLoader();
+        if (threadContextClassLoader != null) {
+            url = threadContextClassLoader.getResource(filename);
+        }
+        if (url == null) {
+            url = Config.class.getResource(filename);
+            if (url == null) {
+                log.warning("OSCache: No properties file found in the classpath by filename " + filename);
+                return new Properties();
+            }
+        }
+        
+        return loadProperties(url, info);
+    }
 }
