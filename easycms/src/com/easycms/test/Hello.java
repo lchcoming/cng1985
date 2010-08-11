1007ab4f6d67e4b790d3f2d19b37cae6b46e70cd
cng1985 <cng1985@4a583d68-532f-11de-8b28-e1b452efc53a>
2010-08-11 05:03:17 +0000

    开发开始了

---
+++ easycms/src/com/easycms/test/Hello.java
@@ -0,0 +1,21 @@
+package com.easycms.test;
+
+import java.util.Locale;
+
+public class Hello {
+
+	public String m(){
+		return "陈可可";
+	}
+	public void message(){
+		System.out.println("hi i am a");
+	}
+	public void message(String message){
+		System.out.println("hi i am a>>"+message);
+	}
+	public String getH(){
+		return "hi i am H";
+		
+	}
+	
+}
