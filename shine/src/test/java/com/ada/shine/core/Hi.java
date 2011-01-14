f54ba536a6ab609c3e32af2a5016a8bdbec954da
cng1985 <cng1985@4a583d68-532f-11de-8b28-e1b452efc53a>
2011-01-14 03:28:13 +0000

    shine

---
+++ shine/src/test/java/com/ada/shine/core/Hi.java
@@ -0,0 +1,19 @@
+package com.ada.shine.core;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class Hi {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+	@Test
+	public void hi() throws Exception {
+	}
+}
